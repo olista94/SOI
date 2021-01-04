c9fdea75e82de488d14c33865a04bb6b2fc2d389
olista94 <olista94@gmail.com>
2021-01-04 10:51:54 +0100

    Agregado funciones

--- SOI_1_K.sh
+++ SOI_1_K.sh
@@ -1,6 +1,45 @@
 #!/bin/bash
 
-if [ $# -lt 2 ] # || [ $i!=[0-9]+ ]
+# Solicita nombre de dos ficheros y comprueba permiso escritura. Si los tiene, almacena en el fichero recuento el num de lineas, palabras y bytes de cada fichero
+# Si alguno de los dos fichero no existe o no tiene este permiso, muestra el mensaje
+recuento(){
+
+		echo Introduzca el nombre del primer fichero:
+		read fichero1
+		echo Introduzca el nombre del segundo fichero:
+		read fichero2
+		([ -r $fichero1 ] && [ -r $fichero2 ] && wc $fichero1 $fichero2 > recuento) || echo "Alguno o los dos ficheros no tiene permiso de lectura"
+}
+
+# Muestra el mayor y menor numero de los parametros introducidos
+mayorMenor(){
+			menor=$1
+			mayor=$1
+
+			for i in $*
+			do
+				#Comprobacion menor
+				if [ "$i" -le $menor ]
+				then let menor=$i
+				fi
+
+				#Comprobacion mayor
+				if [ "$i" -ge $mayor ]
+				then let mayor=$i
+				fi
+			done
+
+			echo "Menor: $menor"
+			echo "Mayor: $mayor"
+
+}
+
+# Sale del programa
+fin(){
+		echo "Fin del programa"
+}
+
+if [ $# -lt 2 ]
 then echo "Error de sintaxis"
 else
 	opcion="op"
@@ -15,42 +54,10 @@ else
 
 		case $opcion in
 
-		# Solicita nombre de dos ficheros y comprueba permiso escritura. Si los tiene, almacena en el fichero recuento el num de lineas, palabras y bytes de cada fichero
-		# Si alguno de los dos fichero no existe o no tiene este permiso, muestra el mensaje
-		R|r)
-			echo Introduzca el nombre del primer fichero:
-			read fichero1
-			echo Introduzca el nombre del segundo fichero:
-                        read fichero2
-			([ -r $fichero1 ] && [ -r $fichero2 ] && wc $fichero1 $fichero2 > recuento) || echo "Alguno o los dos ficheros no tiene permiso de lectura";;
+			R|r) recuento;;
+			M|m) mayorMenor $*;;
+			F|f) fin;;
 
-		# Muestra el mayor y menor numero de los parametros introducidos
-		M|m)
-			# menor=999999999
-			# mayor=0
-			menor=$1
-			mayor=$1
-
-
-			for i in $*
-			do
-				#Comprobacion menor
-				if [ "$i" -le $menor ]
-				then let menor=$i
-				fi
-
-				#Comprobacion mayor
-        if [ "$i" -ge $mayor ]
-        then let mayor=$i
-        fi
-			done
-
-			echo "Menor: $menor"
-			echo "Mayor: $mayor";;
-
-		# Sale del programa
-		F|f)
-			echo "Fin del programa";;
 		esac
 	done
 fi
